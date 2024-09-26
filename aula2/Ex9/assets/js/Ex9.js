a6c4610eff6f92903586899a328560d5ed6d3d72
Guilherme Romiero <romeiro.gui@hotmail.com>
2024-09-26 09:41:23 -0300

    ex9

--- aula2/Ex9/assets/js/Ex9.js
+++ aula2/Ex9/assets/js/Ex9.js
@@ -3,6 +3,96 @@
 // switchdata exercicio aula 48
 
 
+function zero(num){
+
+return num >= 10 ? num: '0'+num;
+
+
+}
+
+function Semana(diaSemana){
+
+    let diaSemanat 
+
+
+    switch(diaSemanat){
+
+        case 0:
+
+        diaSemanat = "Domingo";
+
+        return diaSemanat;
+
+         case 1:
+
+        diaSemanat = "Segunda";
+
+        return diaSemanat;
+
+        case 2:
+
+        diaSemanat = "Terça";
+
+        return diaSemanat;
+
+        case 3:
+
+        diaSemanat = "Quarta";
+
+        return diaSemanat;
+
+        case 4:
+
+        diaSemanat = "Quinta";
+
+        return diaSemanat;
+
+        case 5:
+
+        diaSemanat = "Sexta";
+
+        return diaSemanat;
+
+        case 6:
+
+        diaSemanat = "Sabado";
+
+        return diaSemanat;
+        
+        default:
+
+        diaSemanat = "";
+
+        return diaSemanat;
+
+
+}
+
+}
+
+
+
+function Data(){
+
+let hora = zero (data.getHours());
+
+let min = zero(data.getMinutes());
+
+let mes = zero(data.getMonth() + 1);
+
+let ano = zero(data.getFullYear());
+
+let diaSemana = zero(data.getDate());
+
+let textoSemana = Semana(diaSemana);
+
+return ""+textoSemana+ ", "+diaSemana+ " de "+mes+" de " +ano+" " +hora+":"+min+"";
+
+
+}
+
+
+
 let resultado = document.querySelector(".resultado");
 
 resultado.innerHTML = "";
@@ -12,9 +102,6 @@ let data = new Date();
 
 console.log(data.toString());
 
+let datacompleta = Data();
 
-resultado.innerHTML += "<p>  hora: "+data+ "  </p>";
-
-
-
-
+resultado.innerHTML += "<p> "+datacompleta+ " </p>";
